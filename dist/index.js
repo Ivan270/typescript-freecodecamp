9f647c3c7103cad211ae44dbb6e70da1dd2c1b5c
Ivan Verdugo <ivan270@gmail.com>
2023-10-10 11:57:48 -0300

    feat: add class implementation
    Including protected, private and public properties, readonly, interface working along with Classes and heritage

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Working with DOM
 var someElement = document.querySelector('.foo');
 // listener
@@ -5,3 +20,44 @@ someElement.addEventListener('blur', function (event) {
     var target = event.target;
     console.log(target.value);
 });
+// Classes
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.unchangeableName = firstName;
+    }
+    User.prototype.getFullName = function () {
+        return "".concat(this.firstName, " ").concat(this.lastName);
+    };
+    return User;
+}());
+var usuario = new User('Juan', 'Perez');
+console.log(usuario.getFullName());
+var Musician = /** @class */ (function () {
+    function Musician(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.unchangeableName = firstName;
+    }
+    Musician.prototype.getFullName = function () {
+        return "".concat(this.firstName, " ").concat(this.lastName);
+    };
+    Musician.maxAge = 50;
+    return Musician;
+}());
+// Heritage
+var Drummer = /** @class */ (function (_super) {
+    __extends(Drummer, _super);
+    function Drummer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Drummer.prototype.setBrand = function (brand) {
+        this.drumBrand = brand;
+    };
+    Drummer.prototype.getBrand = function () {
+        return this.drumBrand;
+    };
+    return Drummer;
+}(Musician));
+var drummer = new Drummer('Joaquin', 'Verdugo');
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,58 @@ someElement.addEventListener('blur', (event) => {
 	const target = event.target as HTMLInputElement;
 	console.log(target.value);
 });
+
+// Classes
+class User {
+	protected firstName: string;
+	private lastName: string;
+	readonly unchangeableName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.unchangeableName = firstName;
+	}
+
+	getFullName(): string {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+const usuario = new User('Juan', 'Perez');
+console.log(usuario.getFullName());
+
+// Interface along with class
+
+interface MusicianInterface {
+	getFullName(): string;
+}
+class Musician implements MusicianInterface {
+	protected firstName: string;
+	private lastName: string;
+	readonly unchangeableName: string;
+	static readonly maxAge = 50;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.unchangeableName = firstName;
+	}
+
+	getFullName(): string {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+// Heritage
+class Drummer extends Musician {
+	private drumBrand: string;
+
+	setBrand(brand: string): void {
+		this.drumBrand = brand;
+	}
+	getBrand(): string {
+		return this.drumBrand;
+	}
+}
+const drummer = new Drummer('Joaquin', 'Verdugo');
